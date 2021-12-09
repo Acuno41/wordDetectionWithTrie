d1faaef44bb9055e27d9fff8e600f684a1c9a548
Onur ACUN <onuracun41@gmail.com>
2021-12-09 15:35:30 +0300

    add read badWords.csv function

--- wordFilter.py
+++ wordFilter.py
@@ -1,7 +1,17 @@
+
 class Trie:
 
+    badWordList = []
     def __init__(self):
         print("Trie Init ! ")
+        
+        self.readBadWords()
+
+    def readBadWords(self):
+        file = open("badWords.csv")
+        self.badWordList = file.read().split("\n")
+        
+        
 
 
 if __name__ == "__main__":
